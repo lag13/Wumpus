cb2699e6204295924061bde52749ed74e873bd28
Lucas Groenendaal <groenendaal92@gmail.com>
2014-08-13 13:17:50 -0400

    Took some code and comments from the text game engine. Still need to fix up comments.

--- wumpusGameEngine.lisp
+++ wumpusGameEngine.lisp
@@ -1,5 +1,28 @@
 ;;;; A game engine for "Hunt the Wumpus" type games.
 
+;; The main two things that this text game engine does is define an
+;; environment in which the "core" of a game's code could be run and
+;; defines a way for coders to add new commands to the game. Besides
+;; that it just defines a couple of general utility functions.
+
+;; In my case, this "environment" takes the form of a plain old
+;; function which I've called "main-game-loop". This function's
+;; parameters are all parameterless functions that will be called by
+;; the game loop and are, naturally, specific to whatever game is
+;; being created. What this function essentially does is read player
+;; input, match it with a command, and execute the function associated
+;; with that command.
+
+;; A command consists of a symbol (which is what the user enters to
+;; execute the command), a function corresponding to the symbol (which
+;; ACTUALLY carries out the command), the number of parameters the
+;; function requires (so the main-game-loop can read those in), and a
+;; string describing what the command does. The only reason I have
+;; that "description" slot is because I liked the idea of being able
+;; to display to a player a list of possible commands and what they
+;; do.
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; How this game engine is structured ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -34,6 +57,44 @@
 ;; losing conditions. 
 
 (load "textGameEngine.lisp")
+
+(defparameter *command-list* nil
+  "List of possible commands that the player can execute.")
+
+(defstruct command sym func num-params description)
+
+;; We must know how many parameters a user created command/function
+;; accepts so that the main loop can read in the appropriate number of
+;; arguments. To prevent the user from having to manually enter the
+;; number I created this macro which will define a function and add it
+;; to the command list.
+(defmacro defcommand (command-sym command-desc command-func arg-list &body body)
+  "Adds a command that a player could execute in a game."
+  `(progn 
+     (push (make-command :sym ',command-sym
+			 :func ',command-func
+			 :num-params ,(length arg-list)
+			 :description ,command-desc)
+	   *command-list*)
+     (defun ,command-func ,arg-list ,@body)))
+
+(defun handle-input (print-prompt)
+  "How input is handled for these types of games."
+  (loop with command with args 
+     until command
+     do
+       (funcall print-prompt)
+       (if (setf command (find (read) *command-list* :key #'command-sym))
+	   (progn	   
+	     (setf args (loop repeat (command-num-params command) 
+			   collect (if (listen)
+				       (read)
+				       (progn
+					 (format t "> ")
+					 (read)))))
+	     (when (listen) (clear-input)) 
+	     (apply (command-func command) args))
+	   (format t "COMMAND NOT FOUND~%"))))
 
 (defvar *player* nil
   "The player character.")
