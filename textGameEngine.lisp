b75c68bd46c577771c69c13fe3fa20bac18568fd
Lucas Groenendaal <groenendaal92@gmail.com>
2014-08-13 13:16:33 -0400

    Removed some code which I no longer think is necessary.

--- textGameEngine.lisp
+++ textGameEngine.lisp
@@ -4,51 +4,7 @@
 ;;; How this game engine is structured ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; The main two things that this text game engine does is define an
-;; environment in which the "core" of a game's code could be run and
-;; defines a way for coders to add new commands to the game. Besides
-;; that it just defines a couple of general utility functions.
-
-;; In my case, this "environment" takes the form of a plain old
-;; function which I've called "main-game-loop". This function's
-;; parameters are all parameterless functions that will be called by
-;; the game loop and are, naturally, specific to whatever game is
-;; being created. What this function essentially does is read player
-;; input, match it with a command, and execute the function associated
-;; with that command.
-
-;; A command consists of a symbol (which is what the user enters to
-;; execute the command), a function corresponding to the symbol (which
-;; ACTUALLY carries out the command), the number of parameters the
-;; function requires (so the main-game-loop can read those in), and a
-;; string describing what the command does. The only reason I have
-;; that "description" slot is because I liked the idea of being able
-;; to display to a player a list of possible commands and what they
-;; do.
-(defstruct command sym func num-params description)
-
-(defparameter *command-list* nil
-  "List of possible commands that the player can execute.")
-
-(defvar *skip-bookkeepingp* nil
-  "A boolean variable that allows the coder to prevent the 'bookkeeping' function in the main-game-loop from being called")
-
-;; We must know how many parameters a user created command/function
-;; accepts so that the main loop can read in the appropriate number of
-;; arguments. To prevent the user from having to manually enter the
-;; number I created this macro which will define a function and add it
-;; to the command list.
-(defmacro defcommand (command-sym command-desc command-func arg-list &body body)
-  "Adds a command that a player could execute in a game."
-  `(progn 
-     (push (make-command :sym ',command-sym
-			 :func ',command-func
-			 :num-params ,(length arg-list)
-			 :description ,command-desc)
-	   *command-list*)
-     (defun ,command-func ,arg-list ,@body)))
-
-(defmacro mgl (&key (continue-game-p T) check-before check-after 
+(defmacro main-game-loop (&key (continue-game-p T) check-before check-after 
 			    pre-comp handle-input post-comp)
   `(tagbody
       (defun skip-pre-comp () (go skip-pre-comp))
@@ -68,32 +24,6 @@
       ,@post-comp
       (go before-handle-input)
     end))
-
-(defun main-game-loop (game-continuesp print-prompt 
-		       print-game-view &optional (extra-bookkeeping (lambda ())))
-  "The main game loop for these types of games."
-  (loop with command with args 
-     while (funcall game-continuesp)
-     do
-       (funcall print-game-view)
-       (setf command nil)
-       (loop until command
-	  do
-	    (funcall print-prompt)
-	    (setf command (find (read) *command-list* :key (lambda (c) (command-sym c))))
-	    (unless command (format t "Command not found.~%")))
-       (setf args (loop repeat (command-num-params command) 
-		     collect (if (listen)
-				 (read)
-				 (progn
-				   (format t "> ")
-				   (read)))))
-					; Clears any remaining input before calling the function.
-       (when (listen) (clear-input)) 
-       (apply (command-func command) args)
-       (if *skip-bookkeepingp*
-	   (setf *skip-bookkeepingp* nil)
-	   (funcall extra-bookkeeping))))
 
 (defun n-rand-unique (n max) 
   "Returns a list of n random unique numbers in the range [0, max)."
