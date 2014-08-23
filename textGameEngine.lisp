126e9d9e5eaed98a890e22cdab349ab2d364544f
Lucas Groenendaal <groenendaal92@gmail.com>
2014-08-23 18:09:50 -0400

    Removed a lot of code and (I think) made it more general.

--- textGameEngine.lisp
+++ textGameEngine.lisp
@@ -1,8 +1,61 @@
 ;;;; A text game engine
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; How this game engine is structured ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Originally when I wrote this text game engine there were two main
+;; things that it defined. One was an environment where the "core"
+;; code of a game could be run. What this environment mainly did was
+;; repeatedly read user input, match it up with a defined command, and
+;; call the function associated with that command (while also reading
+;; any arguments necessary for the funciton). The other was a couple
+;; of macros that allowed programmer's to create the commands that the
+;; "main game loop" would be matching up with user input. I often
+;; change my mind about how to program somthing and this was no
+;; exception.
+
+;; In particular, I thought my implementation of a text game engine
+;; was too specific. If this code could be used for many different
+;; types of games then I shouldn't specify how user input is
+;; handled; I should let the programmer's of specific games deal with
+;; that! So, with that minimalist philosophy in mind, I modified this
+;; file.
+
+;; The main-game-loop is now a macro which essentially pastes passed
+;; in code into a little template. My idea when creating it was that
+;; most games will pretty much follow this pattern: Some computation
+;; could be done before user input, input is handled and action is
+;; taken based on it, some computation could be done after handling
+;; input, and this cycle will repeat based on the state of the
+;; game. In some specific situations you might want to avoid the "pre"
+;; and "post" input computations so, to that end, this macro defines a
+;; couple of functions that could be used to skip these parts. Because
+;; I thought it was simpler to accomplish this "skipping" behavior
+;; using jumps, rather than looping and conditionals, the looping
+;; behavior of the macro is accomplished using the "tagbody" special
+;; operator. Another possible reason for doing this is because, ever
+;; since I did SPIM assembly programming in school, I've always liked
+;; the ability to jump around code. I know it can lead to "spaghetti
+;; code" but the amount of control jumping gives you is just too cool.
+
+;; Note that defining these "skipping" functions within the macro
+;; works for two reasons: One. As far as I can tell by reading the
+;; documentation, wherever defun gets used it defines a global
+;; function. Two. When the functions are defined within the tagbody
+;; like this they capture the value of the tags that they reference
+;; (thats a closure) so even when the functions are called in
+;; different contexts, they'll still reference those specific tags.
+
+;; One reason why I like how this code is written is because to
+;; create an infinite REPL loop I can now just write this:
+
+;;           (main-game-loop :handle-input ((print (eval (read))))) 
+
+;; On some level, I think being able to do that is aesthetically
+;; pleasing but this ability to redifine how input is handled could
+;; make debugging and testing easier. For instance, when I want to
+;; test out my code I could change the :handle-input keyword parameter
+;; to be: ((eval (read))). Then I could directly call functions
+;; associated with user input to simulate normal game behavior, but I
+;; could also manipulate the game state to test out specific
+;; situations.
 
 (defmacro main-game-loop (&key (continue-game-p T) check-before check-after 
 			    pre-comp handle-input post-comp)
@@ -24,29 +77,4 @@
       ,@post-comp
       (go before-handle-input)
     end))
-
-(defun n-rand-unique (n max) 
-  "Returns a list of n random unique numbers in the range [0, max)."
-  (loop with arr = (make-array max 
-			       :initial-contents (loop for i from 0 below max collect i))
-     with lowerBound = (- max n)
-     for i from (1- max) downto lowerBound
-     do (rotatef (aref arr i) (aref arr (random (1+ i))))
-     collect (aref arr i)))
-
-(defun random-list-elem (lst)
-  "Returns a random element from a list."
-  (nth (random (length lst)) lst))
-
-(defun explode-seq (delimiter seq &optional remove-empty-subseqs-p)
-  "Breaks up a sequence into a list of tokens based on the delimiter character."
-  (loop for i = 0 then (1+ j)
-     for j = (position delimiter seq :start i)
-     collect (subseq seq i j) into tokens
-     while j
-     finally
-       (return 
-	 (if remove-empty-subseqs-p
-	     (remove-if (lambda (x) (zerop (length x))) tokens)
-	     tokens))))
 
